dc22d5622e8325dbfa084c99f36753ceee88b50b
cfres <cris.dgrnu@gmail.com>
2022-04-28 17:52:58 +0200

    Functional interface

--- interface.py
+++ interface.py
@@ -1,72 +1,107 @@
 from cgitb import text
 import tkinter as tk
+from tkinter import filedialog
 from webbrowser import get
 
-from sqlalchemy import true
 
-app = tk.Tk()
-app.geometry("900x900")
-app.configure(background="#30323D")
-app.title("CoDeck")
+def browse_files(path):
+    filename = filedialog.askopenfilename(
+        initialdir="./",
+        title="Select a File",
+        filetypes=(
+            ("Python files", "*.py*"),
+            ("all files", "*.*")
+        )
+    )
 
-tk.Label(
-    app, 
-    text="CoDeck Keys Selector", 
-    background="#5C80BC",
-    font=("Ubuntu Mono",26)
-    ).pack(fill=tk.BOTH, expand=True)
+    # Aqui en vez de imprimir guardar
+    print(filename)
+
+    # Change runtime variable to notify the user that the path has
+    # been selected correctly
+    path.set(filename)
 
 
-def create_script_selector(number):
+def save_path(path):
+    # Este path que te llega guardarlo en vez de pintarlo
+    print(path)
+
+
+def create_input(app, number):
+
+    # Create frame to store the widgets
     script_frame = tk.Frame(app, height=10, padx=30, pady=15)
     script_frame.pack(fill="both", expand=True)
 
-    path1 = tk.StringVar(app)
+    # Variable to store the path on runtime
+    path = tk.StringVar(
+        app, value=f'Cargar desde SQLite el path al script {number}')
 
+    # Create widgets
+    create_label(script_frame,number+1)
+    create_button(script_frame, "#5C80BC", "#3B5B91", "left",
+                  "Find", lambda: browse_files(path))
+    create_button(script_frame, "#F9627D", "#F7264D", "right",
+                  "Save", lambda: save_path(path.get()))
+    create_entry(script_frame, path)
+
+
+def create_label(frame, number):
     tk.Label(
-        script_frame,
-        text="Script "+str(number) + " path",
-        font=("Ubuntu Mono",12)
-        ).pack(side="left", padx=10)
+        frame,
+        text=f'Script {number} path',
+        font=("Ubuntu Mono", 12)
+    ).pack(side="left", padx=10)
 
+def create_button(frame, bg_color, fg_color, side, text, funct_call):
     tk.Button(
-        script_frame,
-        background="#5C80BC",
-        activebackground="#3B5B91",
+        frame,
+        background=bg_color,
+        activebackground=fg_color,
         activeforeground="#FFFFFF",
         borderwidth=0,
-        text="Find",
-        font=("Ubuntu Mono",10),
-        command=lambda: print(path1.get())
-    ).pack(side="left")
+        text=text,
+        font=("Ubuntu Mono", 10),
+        command=funct_call
+    ).pack(side=side)
 
-    tk.Button(
-        script_frame,
-        text="Save",
-        background="#F9627D",
-        activebackground="#F7264D",
-        activeforeground="#FFFFFF",
-        borderwidth=0,
-        width=7,
-        font=("Ubuntu Mono",10),
-        command=lambda: print(path1.get())
-    ).pack(side="right")
-
+def create_entry(frame, path):
     tk.Entry(
-        script_frame,
+        frame,
         background="#4D5061",
         foreground="#FFFFFF",
-        textvariable=path1,
+        textvariable=path,
         width=52,
         borderwidth=0,
-        font=("Ubuntu Mono",12),
+        font=("Ubuntu Mono", 12),
     ).pack(side="right", expand=True)
 
-   
+def create_title(app):
+    tk.Label(
+        app,
+        text="CoDeck Keys Selector",
+        background="#5C80BC",
+        font=("Ubuntu Mono", 26)
+    ).pack(fill=tk.BOTH, expand=True)
 
 
-for i in range(0, 8):
-    create_script_selector(i+1)
+def setup_interface():
+    app = tk.Tk()
+    app.geometry("900x900")
+    app.configure(background="#30323D")
+    app.title("CoDeck")
+    return app
 
 
-app.mainloop()
+def main():
+    app = setup_interface()
+    create_title(app)
+  
+    for i in range(0, 8):
+        create_input(app, i+1)
+
+    app.mainloop()
+
+
+if __name__ == "__main__":
+    main()
