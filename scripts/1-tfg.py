c8a82b05bc5481be8a65b525f230e26c3d2aca48
josejustotena0@gmail.com <josejustotena0@gmail.com>
2022-05-03 13:51:29 +0200

    add new scripts

---
+++ scripts/1-tfg.py
@@ -0,0 +1,9 @@
+import subprocess
+import os
+
+# Open VS Code
+os.system("code .")
+
+# Open Intellij Idea
+subprocess.run(['intellij-idea-ultimate'])
+
---
+++ scripts/2-webwork.py
@@ -0,0 +1,4 @@
+#import webbrowser
+import os
+#webbrowser.open('https://www.python.org')
+os.system("google-chrome-stable --new-window https://gmail.com https://calendar.google.com https://meet.google.com")
